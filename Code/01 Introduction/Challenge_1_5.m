76c53ecef1c5616d7c69dfef52b3a0deb28dddd1
jimduggan <jim.duggan@nuigalway.ie>
2019-01-16 11:50:13 +0000

    Updating classroom challenge

---
+++ Code/01 Introduction/Challenge_1_5.m
@@ -0,0 +1,18 @@
+
+% set the number of throws as a constant N
+
+% set the random seed to 100 to ensure replication, using rng()
+
+% create the random throws of 2 dice, using randi([L U],r,c)
+% L - lower value U - upper value r = # rows c = # cols
+
+% sum the two dice (each row) and store in a variable. Use the 
+% MATLAB function sum()
+
+% find all the occurances of the value 7, and display proportion
+% try for 10,100,1000,10000,100000 and 1000000 rolls
+
+% write a loop to calculate the proportion of all outcomes and 
+% store in a vector
+
+
