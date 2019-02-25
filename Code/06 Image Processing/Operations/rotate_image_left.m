7fa6d521cb769318a9640a8c48981c9f2b2bfe95
jimduggan <jim.duggan@nuigalway.ie>
2019-02-25 15:35:24 +0000

    Updating code...

--- Code/06 Image Processing/Operations/rotate_image_left.m
+++ Code/06 Image Processing/Operations/rotate_image_left.m
@@ -2,16 +2,16 @@ I = imread('cameraman.tif');
 
 [r, c] = size(I);
 
-O_90 = uint8(zeros(r,c));
+O_90L = uint8(zeros(r,c));
 
 for i = 1: r
-    O_90(i,:) = fliplr(I(:,i)');
+    O_90L(:,i) = fliplr(I(i,:));
 end
 
 
 
 subplot(3,2,1),imshow(I),title('Original Image');
-subplot(3,2,2),imshow(O_90),title('90 Rotation');
+subplot(3,2,2),imshow(O_90L),title('90 Left Rotation');
 subplot(3,2,3),imshow(I),title('Original Image');
 subplot(3,2,4),imshow(I'),title('Transpose');
 subplot(3,2,5),imshow(I),title('Original Image');
