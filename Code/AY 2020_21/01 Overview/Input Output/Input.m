d7acf58421be8e0aeda31148f0bd6cc8c3ba1ef2
Jim Duggan <jim.duggan@nuigalway.ie>
2021-01-26 07:57:10 +0000

    Update

---
+++ Code/AY 2020_21/01 Overview/Input Output/Input.m
@@ -0,0 +1,13 @@
+clear;
+
+% 's' forces the input to be a character string
+name = input ('Please enter your name: ', 's');
+
+age  = input ('Please enter your age: ');
+
+fprintf("Your name is %s and your age is %d\n",name,age);
+
+
+
+
+
