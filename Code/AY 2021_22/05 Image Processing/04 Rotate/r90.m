875c8655710c609e950d36aeb038c51d07a13632
Jim Duggan <jim.duggan@nuigalway.ie>
2022-02-10 22:32:27 +0000

    Updated simulink code - heater example

--- Code/AY 2021_22/05 Image Processing/04 Rotate/r90.m
+++ Code/AY 2021_22/05 Image Processing/04 Rotate/r90.m
@@ -4,8 +4,8 @@ flip = @(m)rot90(rot90(m));
 
 f_C = flip(C);
 
-subplot(1,2,1); imshow(C); title('Original');
-subplot(1,2,2); imshow(f_C); title('Flipped');
+subplot(2,1,1); imshow(C); title('Original');
+subplot(2,1,2); imshow(f_C); title('Flipped');
 
 
 
