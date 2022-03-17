8eceabd221787b7ed6606db1623dc3f516389ebc
Jim Duggan <jim.duggan@nuigalway.ie>
2022-03-17 11:00:28 +0000

    Updating aimsir17 with day number

--- Code/AY 2021_22/11 Data Science III/load_a17.m
+++ Code/AY 2021_22/11 Data Science III/load_a17.m
@@ -6,3 +6,9 @@ subplot(1,3,2);
 boxplot(a17.temp,a17.month);
 subplot(1,3,3);
 boxplot(a17.temp,a17.station);
+
+[G, station, month] = findgroups(a17.station,a17.month);
+
+TotalRain = splitapply(@sum,a17.rain,G);
+
+res = table(station, month, TotalRain);
