240b28b91775e29380d0a685b2e0a60943bd5e23
Jim Duggan <jim.duggan@nuigalway.ie>
2023-02-27 16:52:33 +0000

    Create run_model.m

---
+++ Code/AY 2022_23/08 Modelling 2/run_model.m
@@ -0,0 +1,8 @@
+% (1) create an m-file model for dp/dt=rP
+% (2) debug the model function, see the values of t and x
+% (3) Call the function and vary the growth fraction
+% (4) Call it multiple times and store the results
+% (5) Show the analytical results fpr thos model
+
+
+
