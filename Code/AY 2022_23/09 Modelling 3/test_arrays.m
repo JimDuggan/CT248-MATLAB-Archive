13b80ca211d44d21804386e1995fa416f963b1bf
Jim Duggan <jim.duggan@nuigalway.ie>
2023-03-06 17:23:09 +0000

    Update test_arrays.m

--- Code/AY 2022_23/09 Modelling 3/test_arrays.m
+++ Code/AY 2022_23/09 Modelling 3/test_arrays.m
@@ -1,20 +1,37 @@
 % Code to create an output data structure in advance
 clear;
+
+% define dP/dt = rP, exponential growth (or decline)
 f = @(t,x,r) r*x(1);
 
-N_States = 5;
-runs = linspace(.07,.20,N_States);
+% define number of scenarios
+N_Scenarios = 50;
+
+% define the parameter that will be varied
+runs = linspace(.07,.50,N_Scenarios);
+
+% define the time vector
 time_vec = 0:10;
+
 % Create the output array in advance and initialise
-y_out = zeros(length(time_vec),N_States);
-init_vec = zeros(1,N_States);
+y_out = zeros(length(time_vec),N_Scenarios);
+
+% Create the initial vectors and initialise (100)
 init_vec = 100;
 
-for i = 1:N_States
-    [t,y] = ode45(f,time_vec,init_vec,odeset,runs(i));
+% run the model N_Scenario times
+for i = 1:N_Scenarios
+    [t,y] = ode45(f,                ...
+                         time_vec,   ...
+                         init_vec,     ...
+                         odeset,      ...
+                         runs(i));
+
+    % store the result in column i
     y_out(:,i) = y;
 end
 
+% Plot the results
 plot(t,y_out);
 
 
